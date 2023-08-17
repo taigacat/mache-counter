01d8ffeec3faa9d95984278b5fb614d1ab8a31c5
taigacat <taigacat@users.noreply.github.com>
2023-08-17 16:30:14 +0900

    ドラッグで要素を移動できるようにする

--- src/pages/content/features/gift-counter/gift-table/GiftTable.tsx
+++ src/pages/content/features/gift-counter/gift-table/GiftTable.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Draggable from 'react-draggable';
 import {useSelector} from 'react-redux';
 import {giftSummerySelector} from '../gift-counter.selector';
 import {GiftItem} from '../gift-item/GiftItem';
@@ -6,12 +7,17 @@ import styles from './GiftTable.module.scss';
 
 export const GiftTable: React.FC = () => {
   const gifts = useSelector(giftSummerySelector)
-
+  const windowHeight = window.innerHeight;
   return (
-    <div className={styles['gift-table']}>
-      {gifts.map(gift => (
-        <GiftItem gift={gift} key={gift.name}/>
-      ))}
-    </div>
-  )
+    <Draggable defaultPosition={{x: 10, y: windowHeight - 210}}>
+      <div className={styles['gift-box']}>
+        <div className={styles['gift-header']}></div>
+        <div className={styles['gift-table']}>
+          {gifts.map(gift => (
+            <GiftItem gift={gift} key={gift.name}/>
+          ))}
+        </div>
+      </div>
+    </Draggable>
+  );
 }
--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -3,7 +3,13 @@ import {createRoot} from 'react-dom/client';
 import {App} from './app/App';
 
 
-const root = document.body.appendChild(document.createElement("div"));
+const container = document.createElement('div');
+container.id = 'mache-tools'
+container.style.position = 'absolute';
+container.style.top = '0';
+container.style.left = '0';
+container.style.width = '100%';
+const root = document.body.appendChild(container);
 createRoot(root).render(
   <React.StrictMode>
     <App/>
